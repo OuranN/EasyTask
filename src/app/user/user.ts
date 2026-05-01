4cc9c537cde6c3d659c2b3b13d325ac591edc4fd
Ouran <cristhianjvd@gmail.com>
2026-04-30 23:20:48 -0300

    initial commit

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const RANDOM_INDEX = Math.floor(Math.random()*DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+
+export class User {
+  selectedUser = DUMMY_USERS[RANDOM_INDEX]
+}
